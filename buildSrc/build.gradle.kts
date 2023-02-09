15d23209e20ae4ddef49edc096f2392dddc46d63
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2023-02-09 11:32:58 +0530

    added plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,4 +4,28 @@ plugins {
 
 repositories {
     mavenCentral()
+    google()
+    gradlePluginPortal()
+}
+
+dependencies {
+    compileOnly("com.android.tools.build:gradle:7.3.1")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+}
+
+gradlePlugin {
+    plugins {
+        register("OwlMailApp") {
+            id = "owl-mail-app"
+            implementationClass = "OwlMailAppPlugIn"
+        }
+        register("OwlMailLibrary") {
+            id = "owl-mail-library"
+            implementationClass = "OwlMailLibraryPlugin"
+        }
+        register("OwlMailFeature") {
+            id = "owl-mail-feature"
+            implementationClass = "OwlMailFeaturePlugin"
+        }
+    }
 }
--- buildSrc/src/main/kotlin/OwlMailAppPlugIn.kt
+++ buildSrc/src/main/kotlin/OwlMailAppPlugIn.kt
@@ -1,37 +1,71 @@
+import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 
 class OwlMailAppPlugIn : Plugin<Project> {
 
     private val pluginList = listOf(
+        Plugins.application,
         Plugins.android,
         Plugins.kapt,
         Plugins.hiltAndroid,
         Plugins.playServices,
         Plugins.firebasePerformance,
-        Plugins.firebaseCrashlytics
+        Plugins.firebaseCrashlytics,
+        Plugins.detekt
     )
 
     override fun apply(target: Project) {
         configurePlugins(target)
-        configureLint(target)
-        configureBuildFeatures(target)
-        configureJVM(target)
+        configureAndroid(target)
     }
 
-    private fun configureJVM(project: Project) {
-//        project.extensions.getByType<BaseExtention>()
-    }
+    private fun configureAndroid(project: Project) = with(project) {
+        extensions.configure<ApplicationExtension> {
+            compileSdk = AppConfig.CompileSdk
 
-    private fun configureBuildFeatures(project: Project) {
-    }
+            defaultConfig {
+                minSdk = AppConfig.MinSdk
+                targetSdk = AppConfig.TargetSdk
+                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            }
 
-    private fun configureLint(project: Project) {
-    }
-
-    private fun configurePlugins(project: Project) {
-        pluginList.forEach {
-            project.plugins.apply(it)
+            buildTypes {
+                release {
+                    isMinifyEnabled = true
+                    isShrinkResources = true
+                    proguardFiles(
+                        getDefaultProguardFile("proguard-android-optimize.txt"),
+                        "proguard-rules.pro"
+                    )
+                }
+                debug {
+                    applicationIdSuffix = "debug"
+                }
+//                benchmark {
+//                    // Enable all the optimizations from release build through initWith(release).
+//                    initWith(release)
+//                    matchingFallbacks.add("release")
+//                    // Debug key signing is available to everyone.
+//                    signingConfig = signingConfigs.getByName("debug")
+//                    // Only use benchmark proguard rules
+//                    proguardFiles("benchmark-rules.pro")
+//                    isMinifyEnabled = true
+//                    applicationIdSuffix = "benchmark"
+//                }
+            }
+            compileOptions {
+                sourceCompatibility(AppConfig.JDKVersion)
+                targetCompatibility(AppConfig.JDKVersion)
+            }
+            viewBinding {
+                enable = true
+            }
         }
+    }
+
+    private fun configurePlugins(project: Project) = with(project.pluginManager) {
+        pluginList.forEach { apply(it) }
     }
 }
